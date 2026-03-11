fdb5db3cd1db7f426658d81a5fc7f61a5a09d624
Mubashir ahammed <mymac@Mubashirs-MacBook-Air.local>
2026-03-11 12:00:42 +0530

    meta pixel id problem fixed

--- src/lib/crypto-polyfill.ts
+++ src/lib/crypto-polyfill.ts
@@ -10,9 +10,10 @@ if (
     globalThis.crypto.getRandomValues(bytes);
     bytes[6] = (bytes[6] & 0x0f) | 0x40; // version 4
     bytes[8] = (bytes[8] & 0x3f) | 0x80; // variant
-    return (bytes.reduce((acc, b, i) => {
+    const uuid = bytes.reduce((acc, b, i) => {
       const hex = b.toString(16).padStart(2, '0');
       return acc + ([4, 6, 8, 10].includes(i) ? '-' : '') + hex;
-    }, '') as `${string}-${string}-${string}-${string}-${string}`);
+    }, '');
+    return uuid as unknown as `${string}-${string}-${string}-${string}-${string}`;
   };
 }
